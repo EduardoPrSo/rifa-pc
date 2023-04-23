518dec86ce623bcd3b63a2acb4afd1ed619b2330
DuDuPrSo <85527054+EduardoPrSo@users.noreply.github.com>
2023-04-23 16:34:21 -0300

    Update webhook.js

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -5,17 +5,19 @@ export default async function handler(req, res){
 
     const { body } = req;
     
-    const notificationCode = body.notificationCode.replace('-', '');
+    const notificationCode = body.notificationCode;
 
-    const request = await fetch(`https://ws.pagseguro.uol.com.br/v3/transactions/notifications/${notificationCode}?email=edup.s@hotmail.com&token=1379F92EA26C4E9BAC3DEBFDEE8E4310`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/xml',
-        }
-    });
+    console.log(notificationCode)
 
-    const response = await request.text();
-    console.log(response)
+    // const request = await fetch(`https://ws.pagseguro.uol.com.br/v3/transactions/notifications/${notificationCode}?email=edup.s@hotmail.com&token=1379F92EA26C4E9BAC3DEBFDEE8E4310`, {
+    //     method: 'GET',
+    //     headers: {
+    //         'Content-Type': 'application/xml',
+    //     }
+    // });
+
+    // const response = await request.text();
+    // console.log(response)
 
     res.status(200).json({ message: 'success' })
 }
