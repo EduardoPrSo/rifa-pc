c263c80f402d6419fff4db8fff3abe184116a85e
DuDuPrSo <85527054+EduardoPrSo@users.noreply.github.com>
2023-04-23 20:50:39 -0300

    Update webhook.js

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -7,35 +7,35 @@ export default async function handler(req, res){
     const { body } = req;
     
     const notificationCode = body.notificationCode;
-    const apiURL = `${process.env.NEXT_PAGSEGURO_PUBLIC_WEBHOOK_URL}/${notificationCode}?email=${process.env.NEXT_PUBLIC_PAGSEGURO_EMAIL}&token=${process.env.NEXT_PUBLIC_PAGSEGURO_TOKEN}`
-    console.log(notificationCode)
+    const apiURL = `${process.env.NEXT_PUBLIC_PAGSEGURO_WEBHOOK_URL}/${notificationCode}?email=${process.env.NEXT_PUBLIC_PAGSEGURO_EMAIL}&token=${process.env.NEXT_PUBLIC_PAGSEGURO_TOKEN}`
+    console.log(body, apiURL)
 
-    const request = await fetch(apiURL, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/xml',
-        }
-    });
+    // const request = await fetch(apiURL, {
+    //     method: 'GET',
+    //     headers: {
+    //         'Content-Type': 'application/xml',
+    //     }
+    // });
 
-    const response = await request.text();
-    const dom = new JSDOM(response);
+    // const response = await request.text();
+    // const dom = new JSDOM(response);
     
-    const reference = dom.window.document.getElementsByTagName('reference')[0].textContent;
-    const status = dom.window.document.getElementsByTagName('status')[0].textContent;
+    // const reference = dom.window.document.getElementsByTagName('reference')[0].textContent;
+    // const status = dom.window.document.getElementsByTagName('status')[0].textContent;
 
-    if (status !== '3') {
-        console.log('Parou aqui', status)
-        res.status(200).json({ message: 'success' })
-        return;
-    }
+    // if (status !== '3') {
+    //     console.log('Parou aqui', status)
+    //     res.status(200).json({ message: 'success' })
+    //     return;
+    // }
 
-    try {
-        await conn.query(`UPDATE payments SET status = '1' WHERE reference = '${reference}'`);
-    } catch(err) {
-        console.log(err);
-    } finally {
-        await conn.end();
-    }
+    // try {
+    //     await conn.query(`UPDATE payments SET status = '1' WHERE reference = '${reference}'`);
+    // } catch(err) {
+    //     console.log(err);
+    // } finally {
+    //     await conn.end();
+    // }
 
     res.status(200).json({ message: 'success' })
 }
