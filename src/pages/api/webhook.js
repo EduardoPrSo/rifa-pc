7bac24bdcf06acdee6675754742c8bc6af07b7c6
DuDuPrSo <85527054+EduardoPrSo@users.noreply.github.com>
2023-04-23 03:57:52 -0300

    Update webhook.js

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -7,7 +7,7 @@ export default async function handler(req, res){
     
     const notificationCode = body.notificationCode;
 
-    const request = await fetch(`https://ws.pagseguro.uol.com.br/v3/transactions/notifications/${notificationCode}?email=edup.s@hotmail.com&token=1379F92EA26C4E9BAC3DEBFDEE8E4310`, {
+    const request = await fetch(`https://ws.pagseguro.uol.com.br/v3/transactions/notifications/{${notificationCode}}?email={edup.s@hotmail.com}&token={1379F92EA26C4E9BAC3DEBFDEE8E4310}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
