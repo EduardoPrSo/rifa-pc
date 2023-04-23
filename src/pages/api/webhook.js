ae0c67b577cec3b9875922854b7ee1645b9cb3dc
DuDuPrSo <85527054+EduardoPrSo@users.noreply.github.com>
2023-04-23 20:28:10 -0300

    Update webhook.js

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -7,8 +7,10 @@ export default async function handler(req, res){
     const { body } = req;
     
     const notificationCode = body.notificationCode;
+    const apiURL = `${process.env.NEXT_PUBLIC_WEBHOOK_URL}/${notificationCode}?email=${process.env.NEXT_PUBLIC_PAGSEGURO_EMAIL}&token=${process.env.NEXT_PUBLIC_PAGSEGURO_TOKEN}`
+    console.log(apiURL)
 
-    const request = await fetch(`${process.env.NEXT_PUBLIC_WEBHOOK_URL}/${notificationCode}?email=${process.env.NEXT_PUBLIC_PAGSEGURO_EMAIL}&token=${process.env.NEXT_PUBLIC_PAGSEGURO_TOKEN}`, {
+    const request = await fetch(apiURL, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/xml',
@@ -22,12 +24,13 @@ export default async function handler(req, res){
     const status = dom.window.document.getElementsByTagName('status')[0].textContent;
 
     if (status !== '3') {
+        console.log('Parou aqui', status)
         res.status(200).json({ message: 'success' })
         return;
     }
 
     try {
-        await conn.query(`UPDATE payments SET status = 1 WHERE reference = ?`, [reference]);
+        await conn.query(`UPDATE payments SET status = '1' WHERE reference = '${reference}'`);
     } catch(err) {
         console.log(err);
     } finally {
