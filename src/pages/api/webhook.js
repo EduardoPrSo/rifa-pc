cf9073f901cbebcdfce29c31e5c6c2daeea28d48
DuDuPrSo <85527054+EduardoPrSo@users.noreply.github.com>
2023-04-23 03:45:46 -0300

    Update webhook.js

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -14,7 +14,7 @@ export default async function handler(req, res){
         }
     });
 
-    const response = await request.json();
+    const response = await request.text();
     console.log(response)
 
     res.status(200).json({ message: 'success' })
