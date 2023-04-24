6b8a4448ccd94c725a7a269775605a976c05ae5c
DuDuPrSo <85527054+EduardoPrSo@users.noreply.github.com>
2023-04-23 21:02:58 -0300

    Update webhook.js

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -6,9 +6,23 @@ export default async function handler(req, res){
 
     const { body } = req;
     
-    const notificationCode = body.notificationCode;
-    const apiURL = `${process.env.NEXT_PUBLIC_PAGSEGURO_WEBHOOK_URL}/${notificationCode}?email=${process.env.NEXT_PUBLIC_PAGSEGURO_EMAIL}&token=${process.env.NEXT_PUBLIC_PAGSEGURO_TOKEN}`
-    console.log(body, apiURL)
+    if (body.charges.status !== 'PAID') {
+        res.status(200).json({ message: 'success' })
+        return;
+    }
+
+    try {
+        await conn.query(`UPDATE payments SET status = '1' WHERE reference = '${body.reference_id}' AND transaction_id = '${body.id}'`);
+    } catch(err) {
+        console.log(err);
+    } finally {
+        await conn.end();
+    }
+
+    res.status(200).json({ message: 'success' })
+
+    // const notificationCode = body.notificationCode;
+    // const apiURL = `${process.env.NEXT_PUBLIC_PAGSEGURO_WEBHOOK_URL}/${notificationCode}?email=${process.env.NEXT_PUBLIC_PAGSEGURO_EMAIL}&token=${process.env.NEXT_PUBLIC_PAGSEGURO_TOKEN}`
 
     // const request = await fetch(apiURL, {
     //     method: 'GET',
@@ -22,20 +36,4 @@ export default async function handler(req, res){
     
     // const reference = dom.window.document.getElementsByTagName('reference')[0].textContent;
     // const status = dom.window.document.getElementsByTagName('status')[0].textContent;
-
-    // if (status !== '3') {
-    //     console.log('Parou aqui', status)
-    //     res.status(200).json({ message: 'success' })
-    //     return;
-    // }
-
-    // try {
-    //     await conn.query(`UPDATE payments SET status = '1' WHERE reference = '${reference}'`);
-    // } catch(err) {
-    //     console.log(err);
-    // } finally {
-    //     await conn.end();
-    // }
-
-    res.status(200).json({ message: 'success' })
 }
