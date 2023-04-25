82590fd181df11ce946227bc97e9d02c1b667dcc
DuDuPrSo <85527054+EduardoPrSo@users.noreply.github.com>
2023-04-24 21:23:57 -0300

    Update checkPayment.js

--- src/pages/api/checkPayment.js
+++ src/pages/api/checkPayment.js
@@ -2,11 +2,12 @@ import { database } from "@/services/db"
 
 export default async function handler(req, res) {
     const conn = await database();
+    const { body } = req;
 
     try {
         let status = await conn.query(`SELECT status FROM payments WHERE reference = '${body.reference}'`)
         status = status[0][0].status;
-        console.log(status)
+        
         res.status(200).json({ status })
         return
     } catch(err) {
